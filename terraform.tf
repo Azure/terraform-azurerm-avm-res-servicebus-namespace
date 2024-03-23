772a7442e3248828fae27e18f112f5a90fea1d70
Bryan <117635118+bryansan-msft@users.noreply.github.com>
2024-03-23 19:09:36 +0100

    Added missing required provider

--- terraform.tf
+++ terraform.tf
@@ -2,9 +2,15 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.71"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 }
