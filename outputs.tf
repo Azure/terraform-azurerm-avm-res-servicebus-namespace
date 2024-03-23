74c3469010c8ade3f13efa4e13e98e43fd9013c0
Bryan Sanchez <bryansan@microsoft.com>
2024-03-23 23:19:51 +0100

    Added missing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,63 @@
-output "private_endpoints" {
+output "resource" {
+  value       = azurerm_servicebus_namespace.this
+  description = "The service bus namespace created"
+  sensitive   = true
+}
+
+output "resource_authorization_rules" {
+  value       = azurerm_servicebus_namespace_authorization_rule.this
+  description = "The service bus namespace authorization rules created"
+  sensitive   = true
+}
+
+output "resource_diagnostic_settings" {
+  value       = azurerm_monitor_diagnostic_setting.this
+  description = "The diagnostic settings created"
+}
+
+output "resource_role_assignments" {
+  value       = azurerm_role_assignment.this
+  description = "The role assignments created"
+}
+
+output "resource_locks" {
+  value       = azurerm_management_lock.this
+  description = "The management locks created"
+}
+
+output "resource_private_endpoints" {
   value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this_managed_dns_zone_groups : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
-  description = <<DESCRIPTION
-      A map of the private endpoints created.
-    DESCRIPTION
+  description = "A map of the private endpoints created."
+}
+
+output "resource_private_endpoints_application_security_group_association" {
+  value       = azurerm_private_endpoint_application_security_group_association.this
+  description = "The private endpoint application security group associations created"
+}
+
+output "resource_queues" {
+  value       = azurerm_servicebus_queue.this
+  description = "The service bus queues created"
+}
+
+output "resource_queues_authorization_rules" {
+  value       = azurerm_servicebus_queue_authorization_rule.this
+  description = "The service bus queues authorization rules created"
+  sensitive   = true
+}
+
+output "resource_topics" {
+  value       = azurerm_servicebus_topic.this
+  description = "The service bus topics created"
+}
+
+output "resource_topics_authorization_rules" {
+  value       = azurerm_servicebus_topic_authorization_rule.this
+  description = "The service bus topics authorization rules created"
+  sensitive   = true
+}
+
+output "resource_topics_subscriptions" {
+  value       = azurerm_servicebus_subscription.this
+  description = "The service bus topic subscriptions created"
 }
